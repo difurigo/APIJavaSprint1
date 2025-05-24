8f7dfc843c883e1cc70c513a0d07f2287990e5eb
difurigo <furigo9diego@gmail.com>
2025-05-24 17:30:42 -0300

    Acho que vai dar certo

--- deploy.sh
+++ deploy.sh
@@ -6,20 +6,17 @@ LOCATION="brazilsouth"
 ADMIN_USER="usuario"
 ADMIN_PASSWORD="Senha123!DevOps"
 
-# Criar grupo de recursos
 az group create --name $RESOURCE_GROUP --location $LOCATION
 
-# Criar VM com imagem Ubuntu2204 (confirmadamente suportada)
 az vm create \
   --resource-group $RESOURCE_GROUP \
   --name $VM_NAME \
-  --image Ubuntu2204 \
+  --image Canonical:UbuntuServer:18.04-LTS:latest \
   --size Standard_B2s \
   --admin-username $ADMIN_USER \
   --admin-password $ADMIN_PASSWORD \
   --authentication-type password
 
-# Liberar porta 8080
 az vm open-port \
   --resource-group $RESOURCE_GROUP \
   --name $VM_NAME \
